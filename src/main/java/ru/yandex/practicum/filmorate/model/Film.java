6477a5434c050a4f2723b5a50d5be75c6b11c02c
Vadim Katkov <vmalyh777@gmail.com>
2024-09-22 20:05:14 +0300

    Добавить модель данных

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,20 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+
+import java.time.Duration;
+import java.time.LocalDate;
 
 /**
  * Film.
  */
-@Getter
-@Setter
+@Data
 public class Film {
+    int id;
+    String name;
+    String description;
+    @JsonFormat(pattern = "dd.MM.yyyy HH:mm:ss")
+    LocalDate releaseDate;
+    Duration duration;
 }
---
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -0,0 +1,17 @@
+package ru.yandex.practicum.filmorate.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+
+@Data
+public class User {
+    int id;
+    String email;
+    String login;
+    String name;
+    @JsonFormat(pattern="dd.MM.yyyy")
+    LocalDate birthday;
+}
