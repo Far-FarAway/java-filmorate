b2e8a4ead1e97f8604c91f89214a24ccb726e6fe
Vadim Katkov <vmalyh777@gmail.com>
2024-09-27 21:25:37 +0300

    Добавить аннотации валидации к полям

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,14 +1,26 @@
 package ru.yandex.practicum.filmorate.model;
 
+import ru.yandex.practicum.filmorate.annotation.OnCreate;
+
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 import java.time.LocalDate;
 
 @Data
 public class Film {
+    @PositiveOrZero
     int id;
+    @NotNull(groups={OnCreate.class})
+    @NotBlank(groups={OnCreate.class})
     String name;
+    @NotNull(groups={OnCreate.class})
+    @NotBlank(groups={OnCreate.class})
+    @Size(max = 200)
     String description;
+    @NotNull(groups={OnCreate.class})
+    @PastOrPresent
     LocalDate releaseDate;
+    @PositiveOrZero
     int duration;
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,5 +1,8 @@
 package ru.yandex.practicum.filmorate.model;
 
+import ru.yandex.practicum.filmorate.annotation.OnCreate;
+
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -7,9 +10,16 @@ import java.time.LocalDate;
 
 @Data
 public class User {
+    @PositiveOrZero
     int id;
+    @Email
+    @NotNull(groups={OnCreate.class})
+    @NotBlank(groups={OnCreate.class})
     String email;
+    @NotNull(groups={OnCreate.class})
+    @NotBlank(groups={OnCreate.class})
     String login;
     String name;
+    @PastOrPresent
     LocalDate birthday;
 }
