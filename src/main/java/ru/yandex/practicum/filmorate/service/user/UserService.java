b600575cf564f44bd186cebeb697f9a45a124af4
Vadim Katkov <vmalyh777@gmail.com>
2024-10-22 20:09:19 +0300

    Добавить методы для удаления друзей, просмотра всех и просмотра всех совпадающих

--- src/main/java/ru/yandex/practicum/filmorate/service/user/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/user/UserService.java
@@ -1,21 +1,22 @@
 package ru.yandex.practicum.filmorate.service.user;
 
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.exception.ConditionNotMetException;
 import ru.yandex.practicum.filmorate.model.User;
-import ru.yandex.practicum.filmorate.storage.user.InMemoryUserStorage;
+import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
+import java.util.Collection;
 import java.util.Set;
 import java.util.TreeSet;
 
 @Service
 @Slf4j
 public class UserService {
-    public User addFriend(int userId, int friendId, InMemoryUserStorage userStorage) {
+    public User addFriend(int userId, int friendId, UserStorage storage) {
         log.info("Добавление нового друга");
-        User user = userStorage.findById(userId);
-        User newFriend = userStorage.findById(friendId);
+        User user = storage.findById(userId);
+        User newFriend = storage.findById(friendId);
 
         log.debug("Добавление нового друга(id: {}) в список друзей пользовтеля(id {})", friendId, userId);
         if (user.getFriends() == null) {
@@ -38,5 +39,34 @@ public class UserService {
         log.info("Списки друзей обновлены. Социализация прошла успешно");
         return newFriend;
     }
+
+    public Collection<Integer> getFriends(int userId, UserStorage storage) {
+        log.info("Получение списка друзей пользователя с id {}", userId);
+        return storage.findById(userId).getFriends();
+    }
+
+    public Collection<Integer> getCommonFriends(int userId, int friendId, UserStorage storage) {
+        log.info("Получение списка общих друзей пользователя(id: {}) с другом(id: {})", userId, friendId);
+        User user = storage.findById(userId);
+        User friend = storage.findById(friendId);
+        Set<Integer> friendFriendList = friend.getFriends();
+        return user.getFriends().stream().filter(friendFriendList::contains).toList();
+    }
+
+    public Collection<Integer> deleteUser(int userId, int friendId, UserStorage storage) {
+        log.info("Удаление из списка друзей пользователя(id: {}) друга с id {}", userId, friendId);
+        Set<Integer> userFriendList = storage.findById(userId).getFriends();
+        if(userFriendList.contains(friendId)) {
+            log.info("Пользователь(id: {}) успешно удален из списка друзей", friendId);
+            userFriendList.remove(friendId);
+            return userFriendList;
+        } else {
+            log.warn("Пользователь с id {} не найден в списке друзей пользователя(id {})", friendId, userId);
+            throw new ConditionNotMetException("Пользователь с id " + friendId +
+                    " не найден в списке друзей пользователя(id " + userId + ")");
+        }
+    }
+
+
 
 }
