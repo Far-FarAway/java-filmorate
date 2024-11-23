ca3068f1e9886cd69a36b790cebdb27d46990c38
Tropicus <vmalyh777@gmail.com>
2024-11-23 19:10:47 +0300

    Добавить валидацию данных

--- src/main/java/ru/yandex/practicum/filmorate/model/Friends.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Friends.java
@@ -1,9 +1,12 @@
 package ru.yandex.practicum.filmorate.model;
 
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Data;
 
 @Data
 public class Friends {
+    @PositiveOrZero
     long user_id;
+    @PositiveOrZero
     long friends_id;
 }
