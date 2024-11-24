04137de023f4db404f2680f6b5763b1064b4bc23
Tropicus <vmalyh777@gmail.com>
2024-11-24 04:31:35 +0300

    Добавить маппер фильмов из БД

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmRowMapper.java
@@ -0,0 +1,26 @@
+package ru.yandex.practicum.filmorate.storage.film;
+
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Component
+public class FilmRowMapper implements RowMapper<Film> {
+    @Override
+    public Film mapRow(ResultSet results, int rowNum) throws SQLException {
+        Film film = new Film();
+
+        film.setId(results.getInt("film_id"));
+        film.setName(results.getString("name"));
+        film.setDescription(results.getString("description"));
+        film.setReleaseDate(results.getTimestamp("release_date").toInstant());
+        film.setDuration(results.getInt("duration"));
+        film.setGenre(results.getString("genre"));
+        film.setRating(results.getString("rating"));
+
+        return film;
+    }
+}
