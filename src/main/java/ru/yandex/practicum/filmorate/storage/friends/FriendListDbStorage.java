098edeb29963b074883d798d14d797fd1c967851
Tropicus <vmalyh777@gmail.com>
2024-11-24 16:48:17 +0300

    Добавить класс для работы со списком друзей из БД

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/friends/FriendListDbStorage.java
@@ -0,0 +1,33 @@
+package ru.yandex.practicum.filmorate.storage.friends;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+import ru.yandex.practicum.filmorate.model.FriendList;
+import ru.yandex.practicum.filmorate.storage.BaseRepository;
+
+import java.util.*;
+
+@Repository
+public class FriendListDbStorage extends BaseRepository<FriendList> implements FriendListStorage {
+    private final static String ADD_FRIEND_QUERY = "INSERT INTO friends(user_id, friend_id) VALUES (?, ?)";
+    private final static String DELETE_FRIEND_QUERY = "DELETE FROM friends WHERE user_id = ? AND friend_id = ?";
+    private final static String FIND_FRIENDS_QUERY = "SELECT * FROM friends WHERE user_id = ?";
+
+    public FriendListDbStorage(JdbcTemplate jdbc, @Qualifier("friendListRowMapper") RowMapper<FriendList> mapper) {
+        super(jdbc, mapper);
+    }
+
+    public boolean addFriend(int userId, int newFriendId) {
+        return insert(ADD_FRIEND_QUERY, newFriendId, userId);
+    }
+
+    public boolean deleteFriend(int userId, int friendId) {
+        return delete(DELETE_FRIEND_QUERY, userId, friendId);
+    }
+
+    public Collection<FriendList> getFriends(int userId) {
+        return findMany(FIND_FRIENDS_QUERY, userId);
+    }
+}
