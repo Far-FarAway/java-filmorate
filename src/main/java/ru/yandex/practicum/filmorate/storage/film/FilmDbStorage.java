3e0e816aeeaa6a23dd9b6cece091ccb33e246ac7
Tropicus <vmalyh777@gmail.com>
2024-11-24 04:43:56 +0300

    Исправить sql запрос

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorage.java
@@ -17,7 +17,7 @@ import java.util.Collection;
 public class FilmDbStorage extends BaseRepository<Film> implements FilmStorage {
     private final static String FIND_FILM_QUERY = "SELECT * FROM films WHERE film_id = ?";
     private final static String FIND_FILMS_QUERY = "SELECT * FROM films";
-    private final static String DELETE_FILM_QUERY = "DELETE FROM films WHERE id = ?";
+    private final static String DELETE_FILM_QUERY = "DELETE FROM films WHERE film_id = ?";
     private final static String UPDATE_FILM_QUERY = "UPDATE films SET name = ?, description = ?," +
             " release_date = ?, duration = ?, genre = ?, rating = ?";
     private final static String INSERT_FILM_QUERY = "INSERT INTO films(name, description, release_date, duration," +
