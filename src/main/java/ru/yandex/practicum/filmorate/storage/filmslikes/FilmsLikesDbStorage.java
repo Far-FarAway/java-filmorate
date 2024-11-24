1bfb0c2d640b9ebd8475ceb23831a2131f0de87c
Tropicus <vmalyh777@gmail.com>
2024-11-24 04:36:45 +0300

    Добавить класс для получения данных таблицы лайков фильмов

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/filmslikes/FilmsLikesDbStorage.java
@@ -0,0 +1,66 @@
+package ru.yandex.practicum.filmorate.storage.filmslikes;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+import ru.yandex.practicum.filmorate.model.FilmsLikesModel;
+import ru.yandex.practicum.filmorate.storage.BaseRepository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class FilmsLikesDbStorage extends BaseRepository<FilmsLikesModel> implements FilmsLikes {
+    private final static String ADD_LIKE_QUERY = "INSERT INTO films_likes(film_id, user_id) " +
+            "VALUES (?, ?) returning film_id";
+    private final static String GET_LIKES_QUERY = "SELECT * FROM films_likes";
+    private final static String GET_LIKES_BY_FILM_QUERY = "SELECT * FROM films_likes WHERE film_id = ?";
+    private final static String DELETE_LIKE_QUERY = "DELETE FROM films_likes WHERE film_id = ? and user_id = ?";
+
+    @Autowired
+    public FilmsLikesDbStorage(JdbcTemplate jdbc, @Qualifier("filmsLikesRowMapper") RowMapper<FilmsLikesModel> mapper) {
+        super (jdbc, mapper);
+    }
+
+    public boolean addLike(int filmId, int userId) {
+        return insert(ADD_LIKE_QUERY, filmId, userId);
+    }
+
+    public Map<String, List<String>> getLikes() {
+        Map<String, List<String>> filmsLikes = new HashMap<>();
+        List<String> likesList = null;
+        String filmName = "";
+
+        for(FilmsLikesModel like : findMany(GET_LIKES_QUERY)) {
+            if (!filmName.equals(like.getFilm().getName())) {
+                likesList = new ArrayList<>();
+                filmName = like.getFilm().getName();
+            }
+
+            likesList.add(like.getUser().getName());
+            filmsLikes.put(filmName, likesList);
+        }
+
+        return filmsLikes;
+    }
+
+    public Map<String, List<String>> getLikesByFilm(int filmId) {
+        Map<String, List<String>> filmsLikes = new HashMap<>();
+        List<String> likesList = new ArrayList<>();
+
+        for(FilmsLikesModel like : findMany(GET_LIKES_BY_FILM_QUERY, filmId)) {
+            likesList.add(like.getUser().getName());
+            filmsLikes.put(like.getFilm().getName(), likesList);
+        }
+
+        return filmsLikes;
+    }
+
+    public boolean deleteLike(int filmId, int userId) {
+        return delete(DELETE_LIKE_QUERY, filmId, userId);
+    }
+}
