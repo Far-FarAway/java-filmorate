7f7d0a04df3502af2080fe63594cf57b87f890f5
Tropicus <vmalyh777@gmail.com>
2024-11-26 18:38:22 +0300

    Исправить маппинг жанров

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmRowMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmRowMapper.java
@@ -1,16 +1,23 @@
 package ru.yandex.practicum.filmorate.storage.film;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.Genre;
+import ru.yandex.practicum.filmorate.storage.genre.GenreDbStorage;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.*;
 
 @Component
 @Qualifier("filmRowMapper")
+@RequiredArgsConstructor
 public class FilmRowMapper implements RowMapper<Film> {
+    private final GenreDbStorage genreStorage;
+
     @Override
     public Film mapRow(ResultSet results, int rowNum) throws SQLException {
         Film film = new Film();
@@ -18,10 +25,14 @@ public class FilmRowMapper implements RowMapper<Film> {
         film.setId(results.getInt("film_id"));
         film.setName(results.getString("name"));
         film.setDescription(results.getString("description"));
-        film.setReleaseDate(results.getTimestamp("release_date").toInstant());
+        film.setReleaseDate(results.getTimestamp("release_date").toLocalDateTime().toLocalDate());
         film.setDuration(results.getInt("duration"));
-        film.setGenre(results.getString("genre"));
-        film.setRating(results.getString("rating"));
+
+        Set<Genre> genresList = new HashSet<>();
+        genresList.add(genreStorage.getGenre(results.getInt("genre_id")));
+        film.setGenres(genresList);
+
+        film.setMpa(results.getString("rating"));
 
         return film;
     }
