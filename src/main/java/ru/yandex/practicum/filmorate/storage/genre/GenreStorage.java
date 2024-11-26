d07bbb7228d5a890d57c762731b595c5507aac18
Tropicus <vmalyh777@gmail.com>
2024-11-26 18:59:42 +0300

    Изменить List на Set

--- src/main/java/ru/yandex/practicum/filmorate/storage/genre/GenreStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/genre/GenreStorage.java
@@ -2,10 +2,10 @@ package ru.yandex.practicum.filmorate.storage.genre;
 
 import ru.yandex.practicum.filmorate.model.Genre;
 
-import java.util.List;
+import java.util.Set;
 
 public interface GenreStorage {
     Genre getGenre(int genreId);
-    List<Genre> getGenres();
+    Set<Genre> getGenres();
     boolean deleteGenre(int genreId);
 }
