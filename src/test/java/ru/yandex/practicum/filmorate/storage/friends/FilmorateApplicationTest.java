28f32bbb5610dc12a08a8b3b9e6126e052077b6d
Tropicus <vmalyh777@gmail.com>
2024-11-28 01:52:07 +0300

    Обновить тесты

--- src/test/java/ru/yandex/practicum/filmorate/storage/friends/FilmorateApplicationTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/storage/friends/FilmorateApplicationTest.java
@@ -8,18 +8,24 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.jdbc.core.JdbcTemplate;
-import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.model.FriendList;
-import ru.yandex.practicum.filmorate.model.FriendStatus;
-import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.model.*;
+import ru.yandex.practicum.filmorate.service.film.FilmService;
 import ru.yandex.practicum.filmorate.storage.film.FilmDbStorage;
 import ru.yandex.practicum.filmorate.storage.film.FilmRowMapper;
+import ru.yandex.practicum.filmorate.storage.filmsgenres.FilmsGenresDbStorage;
+import ru.yandex.practicum.filmorate.storage.filmsgenres.FilmsGenresRowMapper;
+import ru.yandex.practicum.filmorate.storage.filmsgenres.FilmsGenresStorage;
 import ru.yandex.practicum.filmorate.storage.filmslikes.FilmsLikesDbStorage;
 import ru.yandex.practicum.filmorate.storage.filmslikes.FilmsLikesRowMapper;
+import ru.yandex.practicum.filmorate.storage.genre.GenreDbStorage;
+import ru.yandex.practicum.filmorate.storage.genre.GenreRowMapper;
+import ru.yandex.practicum.filmorate.storage.mpa.MpaDbStorage;
+import ru.yandex.practicum.filmorate.storage.mpa.MpaRowMapper;
 import ru.yandex.practicum.filmorate.storage.user.UserDbStorage;
 import ru.yandex.practicum.filmorate.storage.user.UserRowMapper;
 
-import java.time.Instant;
+import java.time.LocalDate;
+import java.util.ArrayList;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -27,48 +33,74 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 @AutoConfigureTestDatabase
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 @Import({UserRowMapper.class, UserDbStorage.class, FriendListDbStorage.class, FriendListRowMapper.class,
-         FilmRowMapper.class, FilmDbStorage.class, FilmsLikesDbStorage.class, FilmsLikesRowMapper.class})
+         FilmRowMapper.class, FilmService.class, FilmsLikesDbStorage.class, FilmsLikesRowMapper.class,
+         GenreRowMapper.class, GenreDbStorage.class, MpaRowMapper.class, MpaDbStorage.class,
+         FilmDbStorage.class, FilmsGenresDbStorage.class, FilmsGenresRowMapper.class})
 class FilmoRateApplicationTests {
     private final UserDbStorage userStorage;
     private final FriendListDbStorage friendStorage;
-    private final FilmDbStorage filmStorage;
     private final FilmsLikesDbStorage likesStorage;
+    private final GenreDbStorage genreStorage;
+    private final MpaDbStorage mpaStorage;
+    private final FilmService filmService;
     private final JdbcTemplate jdbc;
     User user1;
     User user2;
     Film film1;
     Film film2;
+    Genre fantasy;
+    Genre comedy;
+    Mpa G;
+    Mpa PG;
 
     @BeforeEach
     public void before() {
+        fantasy = new Genre();
+        fantasy.setName("fantasy");
+        genreStorage.postGenre(fantasy);
+
+        comedy = new Genre();
+        comedy.setName("comedy");
+        genreStorage.postGenre(comedy);
+
+        G = new Mpa();
+        G.setName("G");
+        mpaStorage.postMpa(G);
+
+        PG = new Mpa();
+        PG.setName("PG");
+        mpaStorage.postMpa(PG);
+
         user1 = new User();
         user1.setName("test1");
         user1.setLogin("testUser1");
         user1.setEmail("ewewewe@yandex.ru");
-        user1.setBirthday(Instant.now());
+        user1.setBirthday(LocalDate.now());
         user1.setFriendStatus(FriendStatus.UNCONFIRMED);
 
         user2 = new User();
         user2.setName("test2");
         user2.setLogin("testUser2");
         user2.setEmail("eqweqwe@yandex.ru");
-        user2.setBirthday(Instant.now());
+        user2.setBirthday(LocalDate.now());
         user2.setFriendStatus(FriendStatus.UNCONFIRMED);
 
         film1 = new Film();
         film1.setName("testFilm1");
         film1.setDescription("1Test1");
-        film1.setGenres("Fantasy");
-        film1.setReleaseDate(Instant.now());
-        film1.setMpa("G");
+        film1.setGenres(new ArrayList<>());
+        film1.getGenres().add(fantasy);
+        film1.setReleaseDate(LocalDate.now());
+        film1.setMpa(G);
         film1.setDuration(230);
 
         film2 = new Film();
         film2.setName("testFilm2");
         film2.setDescription("2TestTest2");
-        film2.setGenres("Comedy");
-        film2.setReleaseDate(Instant.now());
-        film2.setMpa("PG");
+        film2.setGenres(new ArrayList<>());
+        film2.getGenres().add(comedy);
+        film2.setReleaseDate(LocalDate.now());
+        film2.setMpa(PG);
         film1.setDuration(20);
 
         jdbc.execute("ALTER TABLE users ALTER COLUMN user_id RESTART WITH 1");
@@ -77,7 +109,7 @@ class FilmoRateApplicationTests {
 
     @Test
     public void testPostUser() {
-        assertThat(userStorage.postUser(user1)).isEqualTo(true);
+        assertThat(userStorage.postUser(user1)).isEqualTo(user1);
     }
 
     @Test
@@ -102,13 +134,14 @@ class FilmoRateApplicationTests {
         updatedUser.setName("updatedData");
         updatedUser.setLogin("updatedAtad");
         updatedUser.setEmail("dsadsadsasadads@yandex.ru");
-        updatedUser.setBirthday(Instant.now());
+        updatedUser.setBirthday(LocalDate.now());
         updatedUser.setFriendStatus(FriendStatus.UNCONFIRMED);
 
         userStorage.postUser(user1);
         userStorage.postUser(user2);
 
-        assertThat(userStorage.updateUser(updatedUser)).isEqualTo(true);
+        userStorage.updateUser(updatedUser);
+
         assertThat(userStorage.findById(updatedUser.getId()))
                 .hasFieldOrPropertyWithValue("login", "updatedAtad");
     }
@@ -128,8 +161,8 @@ class FilmoRateApplicationTests {
         userStorage.postUser(user1);
         userStorage.postUser(user2);
 
-        assertThat(friendStorage.addFriend(1, 2)).isEqualTo(true);
-        assertThat(friendStorage.addFriend(2, 1)).isEqualTo(true);
+        assertThat(friendStorage.addFriend(1, 2)).isEqualTo(-3);
+        assertThat(friendStorage.addFriend(2, 1)).isEqualTo(-3);
     }
 
     @Test
@@ -156,29 +189,31 @@ class FilmoRateApplicationTests {
 
         friendStorage.deleteFriend(2, 1);
 
-        assertThat(friendStorage.getFriends(2).contains(user1)).isEqualTo(false);
+//        /*boolean isContains = */friendStorage.getFriends(2);
+
+        assertThat(friendStorage.getFriends(2).size()).isEqualTo(0);
     }
 
     @Test
     public void testPostFilm() {
-        assertThat(filmStorage.postFilm(film1)).isEqualTo(true);
+        assertThat(filmService.postFilm(film1)).isEqualTo(film1);
     }
 
     @Test
     public void testFindFilmById() {
-        filmStorage.postFilm(film1);
+        filmService.postFilm(film1);
 
-        System.out.println(filmStorage.getFilms());
+        System.out.println(filmService.getFilms());
 
-        assertThat(filmStorage.findById(1)).hasFieldOrPropertyWithValue("id", 1);
+        assertThat(filmService.getFilm(1)).hasFieldOrPropertyWithValue("id", 1);
     }
 
     @Test
     public void testFindFilms() {
-        filmStorage.postFilm(film2);
-        filmStorage.postFilm(film1);
+        filmService.postFilm(film2);
+        filmService.postFilm(film1);
 
-        assertThat(filmStorage.getFilms().size() == 2).isEqualTo(true);
+        assertThat(filmService.getFilms().size() == 2).isEqualTo(true);
     }
 
     @Test
@@ -187,40 +222,39 @@ class FilmoRateApplicationTests {
         updatedFilm.setId(2);
         updatedFilm.setName("updatedData");
         updatedFilm.setDescription("updatedAtad");
-        updatedFilm.setGenres("Romantic");
-        updatedFilm.setReleaseDate(Instant.now());
-        updatedFilm.setMpa("G");
+        updatedFilm.setReleaseDate(LocalDate.now());
         updatedFilm.setDuration(1220);
 
-        filmStorage.postFilm(film1);
-        filmStorage.postFilm(film2);
+        filmService.postFilm(film1);
+        filmService.postFilm(film2);
 
-        assertThat(filmStorage.updateFilm(updatedFilm)).isEqualTo(true);
-        assertThat(filmStorage.findById(updatedFilm.getId()))
-                .hasFieldOrPropertyWithValue("genre", "Romantic");
+        filmService.updateFilm(updatedFilm);
+
+        assertThat(filmService.getFilm(updatedFilm.getId()))
+                .hasFieldOrPropertyWithValue("name", "updatedData");
     }
 
     @Test
     public void testDeleteFilm() {
-        filmStorage.postFilm(film1);
-        filmStorage.postFilm(film2);
+        filmService.postFilm(film1);
+        filmService.postFilm(film2);
 
-        filmStorage.deleteFilm(1);
+        filmService.deleteFilm(1);
 
-        assertThat(filmStorage.getFilms().size() == 1).isEqualTo(true);
+        assertThat(filmService.getFilms().size() == 1).isEqualTo(true);
     }
 
     @Test
     public void testAddLike() {
-        filmStorage.postFilm(film1);
+        filmService.postFilm(film1);
         userStorage.postUser(user1);
 
-        assertThat(likesStorage.addLike(1, 1)).isEqualTo(true);
+        assertThat(likesStorage.addLike(1, 1)).isEqualTo(-3);
     }
 
     @Test
     public void testGetLikes() {
-        filmStorage.postFilm(film1);
+        filmService.postFilm(film1);
         userStorage.postUser(user1);
         userStorage.postUser(user2);
 
@@ -232,8 +266,8 @@ class FilmoRateApplicationTests {
 
     @Test
     public void testGetLikesByFilm() {
-        filmStorage.postFilm(film1);
-        filmStorage.postFilm(film2);
+        filmService.postFilm(film1);
+        filmService.postFilm(film2);
         userStorage.postUser(user1);
         userStorage.postUser(user2);
         userStorage.postUser(user1);
@@ -248,7 +282,7 @@ class FilmoRateApplicationTests {
 
     @Test
     public void testDeleteLike() {
-        filmStorage.postFilm(film1);
+        filmService.postFilm(film1);
         userStorage.postUser(user1);
         userStorage.postUser(user2);
 
